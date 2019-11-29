7816add3eacb015fecbd930bca1fa09040269c9e
琴弦 <735519898@qq.com>
2019-11-29 21:54:33 +0800

    工具包进度更新

---
+++ kite-common/src/main/java/org/aichs/kite/common/interceptor/SecurityConfig.java
@@ -0,0 +1,10 @@
+package org.aichs.kite.common.interceptor;
+
+/**
+ * @ClassName SecurityConfig
+ * @Deacription Security配置类
+ * @Author Kite
+ * @Date 2019/11/29 16:20
+ **/
+public class SecurityConfig {
+}
